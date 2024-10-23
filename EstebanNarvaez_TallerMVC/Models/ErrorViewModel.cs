09461bcb1cea6703631b5ffc1522f1ea91e3768a
Esteban Alejandro Narváez Acurio <esteban.narvaez.acurio@udla.edu.ec>
2024-10-22 23:02:09 -0500

    Agregar archivos de proyecto.

---
+++ EstebanNarvaez_TallerMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EstebanNarvaez_TallerMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
